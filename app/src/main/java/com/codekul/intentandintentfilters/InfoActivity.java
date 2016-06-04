28d2321b773c5cc511990c3b1f1fa8b8b6e8375e
melayer <aniruddha.kudalkar@gmail.com>
2016-06-04 12:13:23 +0530

    initial commit

---
+++ app/src/main/java/com/codekul/intentandintentfilters/InfoActivity.java
@@ -0,0 +1,36 @@
+package com.codekul.intentandintentfilters;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+
+import java.util.Set;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        Intent responsibleIntent = getIntent();
+        String action = responsibleIntent.getAction();
+        Log.i("@codekul","Action - "+action);
+
+        Set<String> setCategories = responsibleIntent.getCategories();
+        for(String category : setCategories){
+            Log.i("@codekul","Category - "+category);
+        }
+
+        Uri uri = responsibleIntent.getData();
+        Log.i("@codekul","Uri - "+uri.toString());
+        Log.i("@codekul","uri fragment - "+uri.getFragment());
+
+
+        final WebView webView = (WebView) findViewById(R.id.webView);
+        webView.loadUrl(uri.toString());
+    }
+}
---
+++ app/src/main/java/com/codekul/intentandintentfilters/MainActivity.java
@@ -0,0 +1,38 @@
+package com.codekul.intentandintentfilters;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnOkay).setOnClickListener(
+                new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                /*Intent intent = new Intent();
+                intent.setAction("com.codekul.action.info");
+                intent.addCategory("com.codekul.category.info");
+                intent.setData(Uri.parse("http://www.codekul.com/android-training-institute-in-pune.html"));
+
+                startActivity(intent);*/
+
+
+                String url = "http://www.codekul.com/android-training-institute-in-pune.html";
+                //Intent i = new Intent(MainActivity.this,NewsActivity.class);
+                Intent i = new Intent();
+                i.setAction(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+    }
+}
